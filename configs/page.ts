c7c7acf11545f29997f71171dda5605ad874bc82
zz15220270326 <826709675@qq.com>
2023-11-01 22:39:31 +0800

    feat(source-transfer-server): 新增七牛云图片替换原来服务器的图片

--- configs/page.ts
+++ configs/page.ts
@@ -30,24 +30,24 @@ const pageConfig = {
   index: {
     title: '首页',
     pageTitle: '张三的资源管理系统',
-    pageIcon: '/imgs/favicon.ico',
+    pageIcon: '//s3ecz0c2b.hn-bkt.clouddn.com/imgs/favicon.ico?e=1698849639&token=WO8DULO9FIsHnPpG2_qXrwSqua2RSfUKL_3li9UE:DfR42lt4Qnjpj9UC-4aKBvAdMAU=',
     pageList: [
       {
         title: '资源列表页面',
         description: '一个可以统一管理资源列表的页面（查询资源列表，查询资源详情，删除资源）',
-        banner: '/imgs/source-list-banner.jpg',
+        banner: '//s3ecz0c2b.hn-bkt.clouddn.com/imgs/source-list-banner.jpg?e=1698848660&token=WO8DULO9FIsHnPpG2_qXrwSqua2RSfUKL_3li9UE:ua3lxJOZbjWNuQpm-a7O3qQwSWQ=',
         href: '/source-list'
       },
       {
         title: 'B站视频上传页面',
         description: '一个可以将B站视频(*.m4s)转化为 MP4 资源的页面（上传 & 转化）',
-        banner: '/imgs/bilibili-banner.jpg',
+        banner: '//s3ecz0c2b.hn-bkt.clouddn.com/imgs/bilibili-banner.jpg?e=1698848693&token=WO8DULO9FIsHnPpG2_qXrwSqua2RSfUKL_3li9UE:A2zsXOTJjLzdToAjSYEhZQpNz_g=',
         href: '/transfer-video'
       },
       {
         title: '网易音乐上传页面',
         description: '一个可以将网易音乐(*.ncm)转化为 MP3 资源的页面（上传 & 转化）',
-        banner: '/imgs/netease-banner.jpg',
+        banner: '//s3ecz0c2b.hn-bkt.clouddn.com/imgs/netease-banner.jpg?e=1698848720&token=WO8DULO9FIsHnPpG2_qXrwSqua2RSfUKL_3li9UE:2XMYcNEcnmWHc7U1_6mkXP0xsNQ=',
         href: '/transfer-audio'
       }
     ],
@@ -55,14 +55,20 @@ const pageConfig = {
       {
         title: '我的语雀博客',
         description: '一些关于张三的个人介绍',
-        banner: '/imgs/blog-banner.jpg',
+        banner: '//s3ecz0c2b.hn-bkt.clouddn.com/imgs/blog-banner.jpg?e=1698848768&token=WO8DULO9FIsHnPpG2_qXrwSqua2RSfUKL_3li9UE:kyGjzjcgkfyuGus-GBd3r0weQgw=',
         href: '//www.yuque.com/jackcheung-pshcv/at8wu2/index',
       },
       {
         title: '我的掘金',
         description: '对外的掘金技术交流',
-        banner: '//p6-passport.byteacctimg.com/img/user-avatar/714b47c68e75465a5c5108aa8a7887b2~300x300.image',
+        banner: '//s3ecz0c2b.hn-bkt.clouddn.com/imgs/juejin-banner.jpg?e=1698849036&token=WO8DULO9FIsHnPpG2_qXrwSqua2RSfUKL_3li9UE:iWv9bVQqFapQAHfaPoeVmcphc9w=',
         href: '//juejin.cn/user/4309726841287277',
+      },
+      {
+        title: '驾照题库-App',
+        description: '一个可以刷驾照考题的 App',
+        banner: '//s3ecz0c2b.hn-bkt.clouddn.com/imgs/jiazhao-test.jpg?e=1698849397&token=WO8DULO9FIsHnPpG2_qXrwSqua2RSfUKL_3li9UE:uHqz5DZ5p5GIQiPCvrUDjzgYa0Q=',
+        href: '//jiazhao-test.bklove-zz.cn'
       }
     ],
     stylesheets: [
