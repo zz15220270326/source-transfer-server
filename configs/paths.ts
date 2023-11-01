bae12c82114dd4b71ad42b28c8435c441e120b13
zz15220270326 <826709675@qq.com>
2023-11-01 13:45:33 +0800

    fix(source-transfer-server): 修复 audios 线上上传路径

--- configs/paths.ts
+++ configs/paths.ts
@@ -54,7 +54,7 @@ export const audioDir = isProduction ? resolve(__dirname, '../../public/audios')
 export const audioImgDir = isProduction ? resolve(__dirname, '../../public/audios/img') : resolve(__dirname, '../public/audios/img');
 
 /** 音频访问的基础路径 */
-export const audioBaseUrl = isProduction ? '/videos/' : `http://${ipv4Address}:9240/audios/`;
+export const audioBaseUrl = isProduction ? '/audios/' : `http://${ipv4Address}:9240/audios/`;
 
 /** 记录音频信息的 JSON 文件路径 */
 export const audioJsonPath = audioDir + '/' + 'data.json';
